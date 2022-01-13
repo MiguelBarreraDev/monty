a2abb6ba0757b83101b5cac3dc7f119c6317ca72
error-rgl <rglcnd@gmail.com>
2022-01-13 18:14:06 +0100

    [:Update:] Add the f_pchar function

--- func_opcode_2.c
+++ func_opcode_2.c
@@ -10,7 +10,7 @@
  */
 void f_mod(stack_t **stack, unsigned int line_number)
 {
-	if ((*stack)->next == NULL)
+	if (!stack || !(*stack) || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
 		band = true;
@@ -23,8 +23,7 @@ void f_mod(stack_t **stack, unsigned int line_number)
 		return;
 	}
 
-	(*stack)->n %= (*stack)->next->n;
-	(*stack)->next->n = (*stack)->n;
+	(*stack)->next->n %= (*stack)->n;
 	(*stack) = (*stack)->next;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
@@ -36,12 +35,19 @@ void f_mod(stack_t **stack, unsigned int line_number)
  * @line_number: line number.
  * Return: no return.
  */
-void f_pchar(_uu_ stack_t **stack, unsigned int line_number)
+void f_pchar(stack_t **stack, unsigned int line_number)
 {
-	if ((*stack)->next == NULL)
+	if (stack == NULL || (*stack) == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		band = true;
+		return;
+	}
+	if ((*stack)->n < 0 || (*stack)->n >= 128)
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 		band = true;
 		return;
 	}
+	printf("%c\n", (*stack)->n);
 }
