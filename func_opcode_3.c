f24c3c3d5d5ff4ce31e323dcf3020fc759b9c468
MiguelBarreraDev <barreram987@gmail.com>
2022-01-13 20:09:41 -0500

    temporary help function

---
+++ func_opcode_3.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * f_trash - black hole
+ *
+ * @stack: head of the linked list.
+ * @line_number: line number.
+ * Return: no return.
+ */
+void f_trash(stack_t **stack, _uu_ unsigned int line_number)
+{
+	(void)stack;
+}
